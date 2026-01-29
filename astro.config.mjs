b562bf970b4f92ee330e0853020852175c682c5a
Nick Matthews <nmatthews@t3rdeyetech.com>
2026-01-29 13:27:28 -0700

    update canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
-const canonicalSite = "https://www.denverblackguide.com";
-
+const canonicalSite = "https://denverblackguide.com";
 const site =
   process.env.CONTEXT === "production"
     ? canonicalSite
@@ -10,11 +8,9 @@ const site =
        process.env.URL ||
        process.env.DEPLOY_PRIME_URL ||
        "http://localhost:4321");
-
 const enableSitemap =
   process.env.CONTEXT === "production" ||
   process.env.GENERATE_SITEMAP === "true";
-
 export default defineConfig({
   site,
   trailingSlash: "always",
